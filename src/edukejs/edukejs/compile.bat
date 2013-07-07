c4ed08be43088833bdfa491f2093640ba7f04f6a
Simon Brown <simonbrown33@gmail.com>
2013-07-07 22:35:28 +0100

    move move non build files into eduke32

---
+++ src/edukejs/edukejs/compile.bat
@@ -0,0 +1,5 @@
+java -jar C:\dev\edukejs\src\edukejs\edukejs\closure-compiler\compiler.jar ^
+ --js C:\dev\edukejs\src\edukejs\edukejs\utils\assert.js^
+ --js C:\dev\edukejs\src\edukejs\edukejs\source\osd.c.js^
+ --js C:\dev\edukejs\src\edukejs\edukejs\source\game.c.js^
+ --js_output_file C:\dev\edukejs\src\edukejs\edukejs\eduke.js
